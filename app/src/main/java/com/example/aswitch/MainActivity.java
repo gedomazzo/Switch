f6eeac819e95b306ac11c439c2c8c6c9435d466f
Nothing Nothing <nothing@nothing>
2025-04-06 12:33:36 +0300

    marrrage

--- app/src/main/java/com/example/aswitch/MainActivity.java
+++ app/src/main/java/com/example/aswitch/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.aswitch;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,10 +16,28 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn;
+    Switch sw;
+    ToggleButton tg;
+
+    TextView txt;
+    LinearLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        btn = (Button) findViewById(R.id.btn);
+        sw = (Switch) findViewById(R.id.sw);
+        tg = (ToggleButton) findViewById(R.id.tg);
+        txt = (TextView) findViewById(R.id.txt);
+        layout = findViewById(R.id.main);
+
+    }
+
+    public void onPush(View view) {
+
     }
 }
